0bddd2305eea17077d15b7a421efe0243306d3f6
Adam G <37425558+aglucky@users.noreply.github.com>
2022-09-03 02:49:09 -0400

    working

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,12 @@
 from wsgiref import headers
 from flask import Flask
-from dotenv import dotenv_values
-import requests
+from dotenv import load_dotenv
+from typeform import Typeform
 import os
 
 app = Flask(__name__)
 
 load_dotenv('.env')
-
 TYPEFORM = os.getenv('TYPEFORM')
 
 @app.route('/start')
@@ -15,23 +14,57 @@ def start():
     url = "https://api.typeform.com/forms"
 
     headers = {}
-    headers["Authorization"] = "Bearer {TWILIO}"
+    headers["Authorization"] = f"Bearer {TYPEFORM}"
 
     form = {}
     form["title"] = "Test Form Carpool"
     form["type"] = "form"
     form["fields"] = [
         {
-            "id": "Name",
             "title": "Name",
             "type": "short_text",
-            "validations": True
+            "validations": {
+                "required": True
+            }
         },
         {
-            "id": "Phone",
             "title": "Phone Number",
             "type": "phone_number",
+            "validations": {
+                "required": True
+            }
+        },
+        {
+            "title": "Pickup Address?",
+            "type": "short_text",
+            "validations": {
+                "required": True
+            }
+        },
+        {
+            "title": "Can you drive?",
+            "type": "yes_no",
+            "validations": {
+                "required": True
+            }
+        },
+        {
+            "title": "If you can drive, how many passengers can you take?",
+            "type": "number",
+            "validations": {
+                "required": True
+            }
         }
     ]
 
-    #resp = requests.post(url, data = form, headers=headers)
+    forms = Typeform(TYPEFORM).forms
+    res = forms.create(form)
+    
+
+    return res['id']
+
+@app.route('/stop/<form_id>')
+def stop(form_id):
+    forms = Typeform(TYPEFORM).forms
+    res = forms.delete(form_id)
+    return res
