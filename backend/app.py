9738f3d05cc83a8cc6fbfad59d038f5dd9662ef7
Adam G <37425558+aglucky@users.noreply.github.com>
2022-09-02 22:48:12 -0400

    new

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,20 @@
+from wsgiref import headers
 from flask import Flask
+from dotenv import dotenv_values
+import requests
+import os
+
 app = Flask(__name__)
+
+load_dotenv('.env')
+
+TYPEFORM = os.getenv('TYPEFORM')
 
 @app.route('/start')
 def start():
-    return 'Hello, World!'
+    url = "https://api.typeform.com/forms"
+
+    headers = {}
+    headers["Authorization"] = "Bearer {TWILIO}"
+
+    #resp = requests.get(url, headers=headers)
