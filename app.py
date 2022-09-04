7dea9b81f4f0e9936dd657387c30fc87a95cd366
Adam G <37425558+aglucky@users.noreply.github.com>
2022-09-03 21:57:16 -0400

    fFA

--- app.py
+++ app.py
@@ -155,3 +155,7 @@ def textMembers(dest, arrTime, groups):
         res = requests.get('https://www.google.com/maps/dir/', params=params)
         print(res.url)
         return True
+
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
